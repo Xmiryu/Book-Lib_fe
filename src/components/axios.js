1a400babc4e19dfbc22fc502655d3000ab1d8af1
Xmiryu <61467204+Xmiryu@users.noreply.github.com>
2021-07-04 02:14:06 +0200

    no message

--- src/components/axios.js
+++ src/components/axios.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
 const production = "https://book-lib-be.herokuapp.com/";
-const local = "http://localhost:8080";
-const url = production;
+const development = "http://localhost:8080";
+
+const url = process.env.NODE_ENV === "production" ? production : development;
+
 axios.defaults.baseURL = `${url}`;
 
 export default axios.create({
